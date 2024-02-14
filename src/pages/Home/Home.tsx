f4ff040db6397fd8e989864567e5312be12b3070
Mykola <nnv2006@ukr.net>
2024-02-14 15:59:48 +0200

    ADD: fonts

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../../components/Title/Title'
+
+const Home = () => {
+    return (
+        <div>
+            Home
+            <Title>What is your preffered language</Title>
+        </div>
+    )
+}
+
+export default Home
