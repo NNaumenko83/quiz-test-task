de4dc53a0331dac6f5ea61224bee30ed74b01707
Mykola <nnv2006@ukr.net>
2024-02-15 17:18:43 +0200

    UPD: PageTwo

--- src/components/GendersList/GenderList.tsx
+++ src/components/GendersList/GenderList.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { GENDERS } from '../../constants/genders'
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router'
 
 const GenderList = () => {
     const { t } = useTranslation()
+    const navigate = useNavigate()
+    const quizString = localStorage.getItem('quiz')
+    const quiz = quizString ? JSON.parse(quizString) : null
+
+    const handleButtonClick: React.MouseEventHandler<
+        HTMLButtonElement
+    > = event => {
+        const gender = event.currentTarget.getAttribute('data-gender')
+        if (gender && quiz) {
+            localStorage.setItem('quiz', JSON.stringify({ ...quiz, gender }))
+            navigate('/quiz/3')
+        }
+    }
+
     return (
         <ul>
             {GENDERS.map(gender => (
                 <li key={gender}>
-                    <button>{t(`${gender}`)}</button>
+                    <button onClick={handleButtonClick} data-gender={gender}>
+                        {t(`${gender}`)}
+                    </button>
                 </li>
             ))}
         </ul>
