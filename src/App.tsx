eb6aaa31f6853004623bf117784e5a0d96495d40
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-02-17 01:15:17 +0200

    UPD

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import { lazy } from 'react'
 import './App.css'
 import './i18n/i18n'
 import SharedLayout from './components/SharedLayout/SharedLayout'
-import Email from './pages/Email/Email'
-import GratitudePage from './pages/GratitudePage/GratitudePage'
-import NotFound from './pages/NotFound/NotFound'
+// import Email from './pages/Email/Email'
+// import GratitudePage from './pages/GratitudePage/GratitudePage'
+// import NotFound from './pages/NotFound/NotFound'
 
 const PageOne = lazy(() => import('./pages/PageOne/PageOne'))
 const PageTwo = lazy(() => import('./pages/PageTwo/PageTwo'))
 const PageThree = lazy(() => import('./pages/PageThree/PageThree'))
 const PageFour = lazy(() => import('./pages/PageFour/PageFour'))
 const PageFive = lazy(() => import('./pages/PageFive/PageFive'))
-// const Email = lazy(() => import('./pages/Email/Email'))
-// const GratitudePage = lazy(() => import('./pages/GratitudePage/GratitudePage'))
-// const NotFound = lazy(() => import('./pages/NotFound/NotFound'))
+const Email = lazy(() => import('./pages/Email/Email'))
+const GratitudePage = lazy(() => import('./pages/GratitudePage/GratitudePage'))
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'))
 
 function App() {
     return (
