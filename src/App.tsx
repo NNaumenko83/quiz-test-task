ac8cb43ccc67548b831fc7866f53aec3ff2d2937
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-02-17 01:13:19 +0200

    UPD: App

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { lazy } from 'react'
 import './App.css'
 import './i18n/i18n'
 import SharedLayout from './components/SharedLayout/SharedLayout'
+import Email from './pages/Email/Email'
+import GratitudePage from './pages/GratitudePage/GratitudePage'
+import NotFound from './pages/NotFound/NotFound'
 
 const PageOne = lazy(() => import('./pages/PageOne/PageOne'))
 const PageTwo = lazy(() => import('./pages/PageTwo/PageTwo'))
 const PageThree = lazy(() => import('./pages/PageThree/PageThree'))
 const PageFour = lazy(() => import('./pages/PageFour/PageFour'))
 const PageFive = lazy(() => import('./pages/PageFive/PageFive'))
-const Email = lazy(() => import('./pages/Email/Email'))
-const GratitudePage = lazy(() => import('./pages/GratitudePage/GratitudePage'))
-const NotFound = lazy(() => import('./pages/NotFound/NotFound'))
+// const Email = lazy(() => import('./pages/Email/Email'))
+// const GratitudePage = lazy(() => import('./pages/GratitudePage/GratitudePage'))
+// const NotFound = lazy(() => import('./pages/NotFound/NotFound'))
 
 function App() {
     return (
@@ -24,11 +27,11 @@ function App() {
                     <Route path="quiz/3" element={<PageThree />} />
                     <Route path="quiz/4" element={<PageFour />} />
                     <Route path="quiz/5" element={<PageFive />} />
-
-                    <Route path="/email" element={<Email />} />
-                    <Route path="/gratitude" element={<GratitudePage />} />
-                    <Route path="*" element={<NotFound />} />
                 </Route>
+
+                <Route path="/email" element={<Email />} />
+                <Route path="/gratitude" element={<GratitudePage />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     )
