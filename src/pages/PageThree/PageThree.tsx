b0388b6b4a1764a01b04c786b9d67d8adce18c8e
Mykola <nnv2006@ukr.net>
2024-02-17 09:57:05 +0200

    UPD: PAgeThree

--- src/pages/PageThree/PageThree.tsx
+++ src/pages/PageThree/PageThree.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next'
 import AgesList from '../../components/AgesList/AgesList'
+import Title from '../../components/Title/Title'
 
 const PageThree = () => {
     const { t } = useTranslation()
     return (
         <div>
-            <h2>{t('what_is_your_age')}</h2>
+            <Title>{t('what_is_your_age')}</Title>
+
             <AgesList />
         </div>
     )
--- src/pages/PageTwo/PageTwo.tsx
+++ src/pages/PageTwo/PageTwo.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import Container from '../../components/Container/Container'
 import GenderList from '../../components/GendersList/GenderList'
 import Title from '../../components/Title/Title'
+import Text from '../../components/Text/Text'
 
 const PageTwo = () => {
     const { t } = useTranslation()
@@ -10,6 +11,7 @@ const PageTwo = () => {
     return (
         <Container>
             <Title>{t('what_gender_do_you_identify_with')}?</Title>
+            <Text>{t('please_share_how_do_you_identify_yourself')}?</Text>
             <GenderList />
         </Container>
     )
