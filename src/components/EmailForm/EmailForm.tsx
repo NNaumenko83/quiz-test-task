10f8f0eff6b77bcdb565d2c36b53989ad35cfe1e
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-02-18 18:21:00 +0200

    UPD: EmailForm

--- src/components/EmailForm/EmailForm.tsx
+++ src/components/EmailForm/EmailForm.tsx
@@ -69,7 +69,7 @@ const EmailForm = () => {
 
     return (
         <>
-            <StyledForm onSubmit={handleSubmit}>
+            <StyledForm onSubmit={handleSubmit} autocomplete="on">
                 <label htmlFor="email">
                     <StyledInput
                         type="email"
